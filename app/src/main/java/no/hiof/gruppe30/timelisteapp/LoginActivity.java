fd8a55f1a0bf6099362dc9e52637be47fa6e171d
Aliha <ali-aliha@hotmail.com>
2018-10-19 20:06:57 +0200

    Firebase implemted

--- app/src/main/java/no/hiof/gruppe30/timelisteapp/LoginActivity.java
+++ app/src/main/java/no/hiof/gruppe30/timelisteapp/LoginActivity.java
@@ -2,18 +2,44 @@ package no.hiof.gruppe30.timelisteapp;
 
 import android.content.Context;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+
 
 public class LoginActivity extends BaseActivity {
 
     Button loginn;
     Button register;
     ProgressBar progresss;
+    EditText username, password;
+    TextView infoa;
+
+    //FireBase
+    //FirebaseApp Fapp;
+    private FirebaseAuth fAuth;
+    FirebaseDatabase database;
+    DatabaseReference users;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +47,15 @@ public class LoginActivity extends BaseActivity {
         setContentView(R.layout.activity_login);
         loginn = findViewById(R.id.LoginBtn);
         progresss = findViewById(R.id.progressBar);
+
+        username = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        infoa = findViewById(R.id.info1);
+
+        fAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = fAuth.getCurrentUser();
+        username.setText(currentUser.getEmail());
+
 
 
         LayoutInflater inflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
@@ -33,6 +68,7 @@ public class LoginActivity extends BaseActivity {
             public void onClick(View view) {
                 progresss.setVisibility(view.VISIBLE);
                 loginn.setVisibility(view.INVISIBLE);
+                signIn(username.getText().toString(), password.getText().toString());
             }
         });
 
@@ -46,5 +82,47 @@ public class LoginActivity extends BaseActivity {
 
 
 
+    }
+
+    private void signIn(final String userN, String password) {
+        fAuth.signInWithEmailAndPassword(userN,password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    // Sign in success, update UI with the signed-in user's information
+
+                    FirebaseUser user = fAuth.getCurrentUser();
+                    infoa.setText("Hello" + user.getEmail());
+
+
+                } else {
+                    // If sign in fails
+                    infoa.setText("user not found" );
+
+                }
+            }
+        });
+
+
+
+
+
+        //Fapp.sign
+
+        /*
+        users.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.child(userN).exists()){
+                    //User login = dataSnapshot.child(userN).getValue(User.class);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+        */
     }
 }
