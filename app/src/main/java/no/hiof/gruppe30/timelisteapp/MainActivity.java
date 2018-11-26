7e0e1f1d3e13484f987a66524f3f321149ebc7fb
Aliha <ali-aliha@hotmail.com>
2018-11-26 09:58:50 +0100

    dashbord

--- app/src/main/java/no/hiof/gruppe30/timelisteapp/MainActivity.java
+++ app/src/main/java/no/hiof/gruppe30/timelisteapp/MainActivity.java
@@ -3,6 +3,8 @@ package no.hiof.gruppe30.timelisteapp;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
@@ -16,6 +18,11 @@ public class MainActivity extends BaseActivity {
     private FirebaseAuth fAuth;
     FirebaseUser user;
     TextView tt;
+    private RecyclerView list;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,11 +34,24 @@ public class MainActivity extends BaseActivity {
         mDrawerLayout.addView(contentView, 0);
 
         tt = findViewById(R.id.t);
+        list = findViewById(R.id.recyclerView);
         fAuth = FirebaseAuth.getInstance();
 
         user = fAuth.getCurrentUser();
 
-        tt.setText("Welcome " + user.getEmail() + "  " + user.getDisplayName());
+        tt.setText("Welcome " + user.getEmail() + " here is your scedule ");
+
+        list.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        list.setLayoutManager(mLayoutManager);
+        // specify an adapter (see also next example)
+        mAdapter = new MyAdapter(myDataset);
+        list.setAdapter(mAdapter);
+
+
+
 
     }
 }
